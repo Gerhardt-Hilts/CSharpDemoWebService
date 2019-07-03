ac37fb5403d0adc72913a14627ea021789750661
Garry-H <gerhardthilts@gmail.com>
2019-07-03 03:58:50 -0700

    Added additional methods

--- SimpleCalculatorApi/Controllers/CalculatorController.cs
+++ SimpleCalculatorApi/Controllers/CalculatorController.cs
@@ -9,12 +9,39 @@ namespace SimpleCalculatorApi.Controllers
     {
         // POST api/calculator/add
         [HttpPost("add")]
-        public ActionResult<string> Post(CalculatorRequest request)
+        public ActionResult<string> Add(CalculatorRequest request)
         {
             var a = request.A;
             var b = request.B;
             
-            return $"A: {a} B: {b}";
+            return $"A: {a} B: {b}, a + b = {a + b}";
+        }
+        // POST api/calculator/subtract
+        [HttpPost("subtract")]
+        public ActionResult<string> Subtract(CalculatorRequest request)
+        {
+            var a = request.A;
+            var b = request.B;
+            
+            return $"A: {a} B: {b}, a + b = {a - b}";
+        }
+        // POST api/calculator/multiply
+        [HttpPost("multiply")]
+        public ActionResult<string> Multiply(CalculatorRequest request)
+        {
+            var a = request.A;
+            var b = request.B;
+            
+            return $"A: {a} B: {b}, a + b = {a * b}";
+        }
+        // POST api/calculator/divide
+        [HttpPost("divide")]
+        public ActionResult<string> Divide(CalculatorRequest request)
+        {
+            var a = request.A;
+            var b = request.B;
+            
+            return $"A: {a} B: {b}, a + b = {a / b}";
         }
     }
 }
